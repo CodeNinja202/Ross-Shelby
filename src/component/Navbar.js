3de2b8641499ca2683274b88718652be06f95e4a
Ross <rosskeene196@gmail.com>
2022-12-03 21:47:36 -0600

    saving template

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -2,9 +2,11 @@ import React from "react";
 
 const Navbar = () => {
   return (
+    <header>
     <div className="navbar-main-div">
      <h1>Shelby's Navbar</h1>
     </div>
+    </header>
   );
 };
 
