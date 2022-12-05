82ac30b4c120ce0c7c04ed93fada32f723f5ae20
Shelby Keene <ShelbyEKeene@gmail.com>
2022-12-04 20:56:20 -0600

    working on styling

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import "./homePage.css";
+import "./pageTwo.css";
+import "./pageThree.css";
+import "./pageFour.css";
+import "./contactus.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
