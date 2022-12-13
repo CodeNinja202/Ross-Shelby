1e44e6b6a7ed5350694f93436e30f22eed288cb7
Ross <rosskeene196@gmail.com>
2022-12-13 16:10:36 -0600

    Navbar Fixed, waffle working

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,37 +1,72 @@
 import React from "react";
 
-import { HashLink as Link  } from "react-router-hash-link";
+import { HashLink as Link } from "react-router-hash-link";
 
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
-
-toggleButton.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active')
-})
+window.onload = function () {
+  const toggleButton = document.getElementsByClassName("toggle-button")[0];
+  const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+  console.log(toggleButton);
+  console.log(navbarLinks);
+  toggleButton.addEventListener("click", () => {
+    navbarLinks.classList.toggle("active");
+  });
+};
 
 const Navbar = () => {
- return (
-
-  <nav className="navbar">
-    <div className="brand-title"><h1>LOGO</h1></div>
-    <a> <Link  href="#" class="toggle-button">
-          <span class="bar"></span>
-          <span class="bar"></span>
-          <span class="bar"></span>
-        </Link >
-        </a>
-    <div className="navbar-links">
-      <ul>
-     <li><a><Link to="#homePageID" smooth>HOME</Link></a></li>
-     <li><a><Link to="#page-two" smooth>PAGE TWO</Link></a></li>
-     <li><a><Link to="#pageThreeID" smooth>PAGE THREE</Link></a></li>
-     <li><a><Link to="#pageFourID" smooth>PAGE FOUR</Link></a></li>
-     <li><a><Link to="#contactUS" smooth>CONTACT</Link></a></li>
-     </ul>
-     </div>
-     </nav>
-  
+  return (
+    <nav className="navbar">
+      <div className="brand-title">
+        <h1>LOGO</h1>
+      </div>
+      <a>
+        {" "}
+        <Link href="#" className="toggle-button">
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
+        </Link>
+      </a>
+      <div className="navbar-links">
+        <ul>
+          <li>
+            <a>
+              <Link to="#homePageID" smooth>
+                HOME
+              </Link>
+            </a>
+          </li>
+          <li>
+            <a>
+              <Link to="#page-two" smooth>
+                PAGE TWO
+              </Link>
+            </a>
+          </li>
+          <li>
+            <a>
+              <Link to="#pageThreeID" smooth>
+                PAGE THREE
+              </Link>
+            </a>
+          </li>
+          <li>
+            <a>
+              <Link to="#pageFourID" smooth>
+                PAGE FOUR
+              </Link>
+            </a>
+          </li>
+          <li>
+            <a>
+              <Link to="#contactUS" smooth>
+                CONTACT
+              </Link>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
-}
+};
 
 export default Navbar;
